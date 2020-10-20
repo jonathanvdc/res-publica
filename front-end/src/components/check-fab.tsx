baff81246a2c968cf6e80e94fb2574159c1d53f7
jonathanvdc <jonathan.vdc@outlook.com>
2020-10-20 16:37:18 +0200

    Create server management page

--- front-end/src/components/check-fab.tsx
+++ front-end/src/components/check-fab.tsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { Button, Fab, FabProps, Theme, withStyles } from "@material-ui/core";
-import { green, red } from "@material-ui/core/colors";
+import { Fab, FabProps, Theme, withStyles } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
 import CheckIcon from '@material-ui/icons/Check';
-
-/**
- * A button that hints danger.
- */
-export const DangerButton = withStyles((theme: Theme) => ({
-    root: {
-        color: theme.palette.getContrastText(red[600]),
-        backgroundColor: red[600],
-        '&:hover': {
-            backgroundColor: red[700],
-        },
-    },
-}))(Button);
 
 const CheckFabTy = withStyles((theme: Theme) => ({
     root: {
