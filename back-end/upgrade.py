486c49e5f709f231df707732a924586b6162a5a9
jonathanvdc <jonathan.vdc@outlook.com>
2020-10-20 18:32:43 +0200

    Git pull locally

--- back-end/upgrade.py
+++ back-end/upgrade.py
@@ -7,20 +7,22 @@ import subprocess
 
 def main():
     """The script's entry point. Takes a path to the config file."""
-    # Run a git pull.
-    subprocess.check_call(['git', 'pull'])
 
-    parent_path = os.path.join(os.path.realpath(__file__), '..')
+    # Compute paths.
+    parent_path = os.path.realpath(os.path.join(os.path.realpath(__file__), '..'))
     front_end_path = os.path.realpath(os.path.join(parent_path, '..', 'front-end'))
 
-    # Build the front-end.
+    # Run a git pull.
+    subprocess.check_call(['git', 'pull'], cwd=parent_path)
+
+    # Install npm packages.
     subprocess.check_call(['npm', 'install'], cwd=front_end_path)
+
+    # Build the front-end.
     subprocess.check_call(['npm', 'run-script', 'build'], cwd=front_end_path)
 
     # Install Python packages.
-    subprocess.check_call(
-        ['pip3', 'install', '-r', 'requirements.txt'],
-        cwd=os.path.realpath(parent_path))
+    subprocess.check_call(['pip3', 'install', '-r', 'requirements.txt'], cwd=parent_path)
 
 if __name__ == "__main__":
     main()
