87f767722d8e3eb34fb493ee224a34d794a6b5d6
jonathanvdc <jonathan.vdc@outlook.com>
2020-10-31 20:09:09 +0100

    Allow for inter-round visualization navigation

--- front-end/src/components/pages/visualize-tally-page.tsx
+++ front-end/src/components/pages/visualize-tally-page.tsx
@@ -1,13 +1,42 @@
-import { PureComponent } from "react";
+import { Button } from "@material-ui/core";
+import React, { Component } from "react";
 
 type Props = {
     rounds: JSX.Element[];
 };
 
-class VisualizeTallyPage extends PureComponent<Props> {
+type State = {
+    currentRound: number;
+};
+
+class VisualizeTallyPage extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { currentRound: 0 };
+    }
+
+    navigate(delta: number) {
+        this.setState({
+            currentRound: this.state.currentRound + delta
+        });
+    }
+
+    renderNavButtons() {
+        return <div style={{display: "flex", width: "100%"}}>
+            {this.state.currentRound > 0 &&
+                <Button variant="contained" onClick={() => this.navigate(-1)}>Previous</Button>}
+            <div style={{flexGrow: 1}} />
+            {this.state.currentRound < this.props.rounds.length - 1 &&
+                <Button variant="contained" onClick={() => this.navigate(1)}>Next</Button>}
+        </div>;
+    }
+
     render() {
-        // TODO: allow user to navigate rounds.
-        return this.props.rounds[0];
+        return <React.Fragment>
+            {this.renderNavButtons()}
+            {this.props.rounds[this.state.currentRound]}
+            {this.renderNavButtons()}
+        </React.Fragment>;
     }
 };
 
