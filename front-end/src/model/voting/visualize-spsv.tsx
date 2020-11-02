30ee10c55af555be266d55e22794dbdf73a40fb2
jonathanvdc <jonathan.vdc@outlook.com>
2020-11-02 16:33:48 +0100

    Tweak virtual ballot visualization box width

--- front-end/src/model/voting/visualize-spsv.tsx
+++ front-end/src/model/voting/visualize-spsv.tsx
@@ -56,7 +56,7 @@ function visualizeCandidate(candidateId: string, round: SPSVRound): JSX.Element 
     return <CandidatePanel isWinner={candidateId === round.roundWinner}>
         <Typography variant="h4">{renderCandidateName(data.option)}</Typography>
         Score: {getCandidateScore(data, round).toFixed(2)}
-        <div style={{display: "flex", flexWrap: "wrap"}}>
+        <div style={{display: "flex", flexWrap: "wrap", minWidth: "2em", width: "100%"}}>
             {Array.of(...ballots.entries()).map(([x, y]) => visualizeBallot(x, y, round))}
         </div>
     </CandidatePanel>;
