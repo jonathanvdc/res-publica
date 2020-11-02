7f49e1488628494fe436f8ca70761a419c234497
jonathanvdc <jonathan.vdc@outlook.com>
2020-11-02 16:23:11 +0100

    Round SPSV scores when visualizing

--- front-end/src/model/voting/visualize-spsv.tsx
+++ front-end/src/model/voting/visualize-spsv.tsx
@@ -55,7 +55,7 @@ function visualizeCandidate(candidateId: string, round: SPSVRound): JSX.Element 
 
     return <CandidatePanel isWinner={candidateId === round.roundWinner}>
         <Typography variant="h4">{renderCandidateName(data.option)}</Typography>
-        Score: {getCandidateScore(data, round)}
+        Score: {getCandidateScore(data, round).toFixed(2)}
         <div style={{display: "flex", flexWrap: "wrap"}}>
             {Array.of(...ballots.entries()).map(([x, y]) => visualizeBallot(x, y, round))}
         </div>
