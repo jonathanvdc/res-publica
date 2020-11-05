a11615a394042e5b4f90a8d787461a1e03f67642
jonathanvdc <jonathan.vdc@outlook.com>
2020-11-05 12:15:51 +0100

    Add SPSV score changes to visualization

--- front-end/src/model/voting/visualize-spsv.tsx
+++ front-end/src/model/voting/visualize-spsv.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
-import { Typography } from "@material-ui/core";
-import { getBallotWeight, getCandidateScore, KotzePereiraBallot, SPSVCandidate, SPSVRound, tallySPSVWithRounds } from "./spsv";
+import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@material-ui/core";
+import { getBallotWeight, getCandidateScore, getCandidateScores, KotzePereiraBallot, SPSVCandidate, SPSVRound, tallySPSVWithRounds } from "./spsv";
 import { RateOptionsBallot, VoteAndBallots } from "./types";
 import CandidatePanel from "../../components/election/candidate-panel";
 import BallotDots from "../../components/election/ballot-dots";
@@ -42,7 +42,126 @@ function visualizeBallot(ballot: RateOptionsBallot, virtualBallots: KotzePereira
             electedCandidates={round.electedCandidates} />} />;
 }
 
-function visualizeCandidate(candidateId: string, round: SPSVRound): JSX.Element {
+/**
+ * The impact of a candidate's election on another candidate.
+ */
+type CandidateImpact = {
+    /**
+     * The ID of the elected candidate.
+     */
+    electedCandidateId: string;
+
+    /**
+     * The other candidate's score before the candidate got elected.
+     */
+    scoreBefore: number;
+
+    /**
+     * The total score before the candidate got elected.
+     */
+    totalScoreBefore: number;
+
+    /**
+     * The other candidate's score after the candidate got elected.
+     */
+    scoreAfter: number;
+
+    /**
+     * The total score after the candidate got elected.
+     */
+    totalScoreAfter: number;
+};
+
+/**
+ * Computes the effect of each previously-elected candidate on each currently-eligible
+ * candidate in a round.
+ * @param round The round to examine.
+ */
+function computeCandidateImpacts(round: SPSVRound): Map<string, CandidateImpact[]> {
+    let candidateScores = new Map<string, number[]>();
+    let totalScores: number[] = [0];
+
+    // First compute initial vote shares.
+    let initialRound: SPSVRound = {
+        ...round,
+        electedCandidates: []
+    };
+    for (let [k, v] of getCandidateScores(initialRound).entries()) {
+        candidateScores.set(k, [v]);
+        totalScores[0] += v;
+    }
+
+    // Then run the rounds, see how pre-elected candidates change the
+    // other candidates' scores.
+    for (let i = 0; i < round.electedCandidates.length; i++) {
+        let roundN: SPSVRound = {
+            ...round,
+            electedCandidates: round.electedCandidates.slice(0, i + 1)
+        };
+        totalScores.push(0);
+        for (let [k, v] of getCandidateScores(roundN).entries()) {
+            candidateScores.get(k)!.push(v);
+            totalScores[i + 1] += v;
+        }
+    }
+
+    // Now repackage the data.
+    let impacts = new Map<string, CandidateImpact[]>();
+    for (let [affectedCandidateId, v] of candidateScores.entries()) {
+        let impactList: CandidateImpact[] = [];
+        for (let i = 0; i < v.length - 1; i++) {
+            impactList.push({
+                electedCandidateId: round.electedCandidates[i],
+                scoreBefore: v[i],
+                scoreAfter: v[i + 1],
+                totalScoreBefore: totalScores[i],
+                totalScoreAfter: totalScores[i + 1]
+            });
+        }
+        impacts.set(affectedCandidateId, impactList);
+    }
+
+    return impacts;
+}
+
+function renderPercentage(value: number, precision: number = 1): string {
+    return `${(value * 100).toFixed(precision)}%`;
+}
+
+function renderCandidateImpactTable(round: SPSVRound, impacts: CandidateImpact[]): JSX.Element {
+    return <Table style={{width: "auto"}}>
+        <TableHead>
+            <TableRow>
+                <TableCell>Elected Candidate</TableCell>
+                <TableCell align="right">Share of Total Score</TableCell>
+                <TableCell align="right">Change</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow key="pre-vote">
+                <TableCell component="th" scope="row" />
+                <TableCell align="right">
+                    {renderPercentage(impacts[0].scoreBefore / impacts[0].totalScoreBefore)}
+                </TableCell>
+                <TableCell align="right" />
+            </TableRow>
+            {impacts.map((impact, i) =>
+                <TableRow key={impact.electedCandidateId}>
+                    <TableCell component="th" scope="row">
+                        {renderCandidateName(idToCandidate(impact.electedCandidateId, round).option)}
+                    </TableCell>
+                    <TableCell align="right">
+                        {renderPercentage(impact.scoreAfter / impact.totalScoreAfter)}
+                    </TableCell>
+                    <TableCell align="right">
+                        {renderPercentage(impact.scoreAfter / impact.totalScoreAfter - impact.scoreBefore / impact.totalScoreBefore)}
+                    </TableCell>
+                </TableRow>)}
+        </TableBody>
+    </Table>;
+}
+
+function visualizeCandidate(candidateId: string, round: SPSVRound, impacts: CandidateImpact[]): JSX.Element {
     let data = round.candidates.find(x => x.option.id === candidateId);
     if (!data) {
         throw new Error(`Cannot find candidate ${candidateId}.`);
@@ -62,9 +181,17 @@ function visualizeCandidate(candidateId: string, round: SPSVRound): JSX.Element 
     return <CandidatePanel isWinner={candidateId === round.roundWinner}>
         <Typography variant="h4">{renderCandidateName(data.option)}</Typography>
         Score: {getCandidateScore(data, round).toFixed(2)}
-        <div style={{display: "flex", flexWrap: "wrap", minWidth: "2em", width: "100%"}}>
+        <div style={{display: "flex", flexWrap: "wrap", minWidth: "2em", width: "100%", alignItems: "center", justifyContent: "center"}}>
             {Array.of(...ballots.entries()).map(([x, y]) => visualizeBallot(x, y, round))}
         </div>
+        {impacts.length > 0 &&
+            <React.Fragment>
+                <hr/>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                    <Typography variant="caption">Explore how previous candidates affected the score.</Typography>
+                    {renderCandidateImpactTable(round, impacts)}
+                </div>
+            </React.Fragment>}
     </CandidatePanel>;
 }
 
@@ -73,10 +200,11 @@ function visualizeCandidate(candidateId: string, round: SPSVRound): JSX.Element 
  * @param round The round to visualize.
  */
 function visualizeRound(round: SPSVRound): JSX.Element {
+    let impacts = computeCandidateImpacts(round);
     return <div>
         <Typography variant="h2">{renderRoundName(round)}</Typography>
-        {visualizeCandidate(round.roundWinner, round)}
-        {round.unelectedCandidates.map(x => visualizeCandidate(x, round))}
+        {visualizeCandidate(round.roundWinner, round, impacts.get(round.roundWinner) || [])}
+        {round.unelectedCandidates.map(x => visualizeCandidate(x, round, impacts.get(x) || []))}
     </div>;
 }
 
